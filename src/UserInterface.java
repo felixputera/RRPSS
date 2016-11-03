4acf99683482a47583b7a800c8e7e82e01acf611
felixputera <felixputera@gmail.com>
2016-11-03 20:59:05 +0800

    Added AlaCarteManager class

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,5 +1,8 @@
 public class UserInterface {
+    //currently main is just used to test AlaCarteManager, remove if necessary
     public static void main(String[] args) {
-
+        AlaCarteManager manage = new AlaCarteManager();
+        manage.addAlaCarte("Choco pie",3,"Snack");
+        manage.printMenu();
     }
 }
