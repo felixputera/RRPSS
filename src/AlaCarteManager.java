978056f0ef732608d4ac0091a34b04d21b2a71c5
felixputera <felixputera@gmail.com>
2016-11-03 22:34:14 +0800

    Reformatted codes

--- src/AlaCarteManager.java
+++ src/AlaCarteManager.java
@@ -9,9 +9,9 @@ public class AlaCarteManager {
         this.menu = (ArrayList) IOHandler.readSerializedObject(FName);
     }
 
-    private int findIndex(int id){
-        for (int i = 0; i < menu.size(); i++){
-            if(menu.get(i).getId() == id) return i;
+    private int findIndex(int id) {
+        for (int i = 0; i < menu.size(); i++) {
+            if (menu.get(i).getId() == id) return i;
         }
         System.out.println("ID not found");
         return -1;
@@ -20,62 +20,62 @@ public class AlaCarteManager {
     public void addAlaCarte(String name, float price, String type) {
         int id;
         if (menu.isEmpty()) id = 1;
-        else{
-            id = menu.get(menu.size()-1).getId()+1;
+        else {
+            id = menu.get(menu.size() - 1).getId() + 1;
         }
         AlaCarteMenu m = new AlaCarteMenu(id, name, price, type);
         this.menu.add(m);
-        IOHandler.writeSerializedObject(FName,menu);
         System.out.println("Added entry to menu");
+        IOHandler.writeSerializedObject(FName, menu);
     }
 
     public void removeAlaCarte(int id) {
         int i = findIndex(id);
-        if(i != -1) {
+        if (i != -1) {
             menu.remove(i);
             System.out.println("Entry deleted");
-            IOHandler.writeSerializedObject(FName,menu);
+            IOHandler.writeSerializedObject(FName, menu);
         }
     }
 
     public void updatePrice(int id, float price) {
         int i = findIndex(id);
-        if(i != -1){
+        if (i != -1) {
             menu.get(i).setPrice(price);
             System.out.println("Price updated");
-            IOHandler.writeSerializedObject(FName,menu);
+            IOHandler.writeSerializedObject(FName, menu);
         }
     }
 
     public void updateName(int id, String name) {
         int i = findIndex(id);
-        if(i != -1){
+        if (i != -1) {
             menu.get(i).setName(name);
             System.out.println("Name updated");
-            IOHandler.writeSerializedObject(FName,menu);
+            IOHandler.writeSerializedObject(FName, menu);
         }
     }
 
     public void updateType(int id, String type) {
         int i = findIndex(id);
-        if(i != -1){
+        if (i != -1) {
             menu.get(i).setType(type);
             System.out.println("Type updated");
-            IOHandler.writeSerializedObject(FName,menu);
+            IOHandler.writeSerializedObject(FName, menu);
         }
     }
 
     public void updateDesc(int id, String desc) {
         int i = findIndex(id);
-        if(i != -1){
+        if (i != -1) {
             menu.get(i).setDesc(desc);
             System.out.println("Description updated");
-            IOHandler.writeSerializedObject(FName,menu);
+            IOHandler.writeSerializedObject(FName, menu);
         }
     }
 
-    public void printMenu(){
-        for(int i = 0 ; i < menu.size() ; i++){
+    public void printMenu() {
+        for (int i = 0; i < menu.size(); i++) {
             AlaCarteMenu x = menu.get(i);
             System.out.println("ID: " + x.getId());
             System.out.println("Name: " + x.getName());
--- src/AlaCarteMenu.java
+++ src/AlaCarteMenu.java
@@ -1,56 +1,56 @@
 import java.io.Serializable;
 
-public class AlaCarteMenu implements Serializable{
+public class AlaCarteMenu implements Serializable {
     private int id;
     private String name;
     private float price;
     private String type;
     private String desc = "";
 
-    public AlaCarteMenu(int id, String name, float price, String type){
+    public AlaCarteMenu(int id, String name, float price, String type) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.type = type;
     }
 
-    public int getId(){
+    public int getId() {
         return this.id;
     }
 
-    public void setId(int id){
+    public void setId(int id) {
         this.id = id;
     }
 
-    public String getName(){
+    public String getName() {
         return this.name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public float getPrice(){
+    public float getPrice() {
         return this.price;
     }
 
-    public void setPrice(float price){
+    public void setPrice(float price) {
         this.price = price;
     }
 
-    public String getType(){
+    public String getType() {
         return this.type;
     }
 
-    public void setType(String type){
+    public void setType(String type) {
         this.type = type;
     }
 
-    public String getDesc(){
+    public String getDesc() {
         return this.desc;
     }
 
-    public void setDesc(String desc){
+    public void setDesc(String desc) {
         this.desc = desc;
     }
 }
